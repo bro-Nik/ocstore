29b69ba9c06fd103ff1e69e7e8973312335e21b3
Nikita Markin <mr.nik2014@gmail.com>
2025-07-31 15:15:08 +0300

    Purge Revolution code

--- catalog/controller/common/home.php
+++ catalog/controller/common/home.php
@@ -39,13 +39,6 @@ class ControllerCommonHome extends Controller {
 		// Revolution
 		// $data['blog'] = $this->load->controller('revolution/revblogmod');
 		$data['blog'] = $this->load->controller('blog/slider');
-		$data['blocks'] = $this->load->controller('revolution/revblocks');
-		$data['socv'] = $this->load->controller('revolution/revsocv');
-		$data['socfb'] = $this->load->controller('revolution/revsocfb');
-		$data['socok'] = $this->load->controller('revolution/revsocok');
-		$data['socvinsta'] = $this->load->controller('revolution/revsocvinsta');
-		$setting_socv = $this->config->get('revtheme_home_socv');
-		$data['right_insta'] = $setting_socv['right_insta'];
 		$data['storereview'] = $this->load->controller('revolution/revstorereviewmod');
 		$data['viewed_products'] = $this->load->controller('revolution/revviewed_products');
 		$setting_allwide = $this->config->get('revtheme_header_menu');
@@ -55,43 +48,13 @@ class ControllerCommonHome extends Controller {
 			$data['allwide'] = false;
 		}
 		$setting_header_menu = $this->config->get('revtheme_header_menu');
-		if ($setting_header_menu['type']) {
+		// if ($setting_header_menu['type']) {
 			$data['amazon'] = true;
-		} else {
-			$data['amazon'] = false;
-		}
+		// } else {
+		// 	$data['amazon'] = false;
+		// }
 		$setting_home_all = $this->config->get('revtheme_home_all');
 		$data['h1_home'] = $setting_home_all[$this->config->get('config_language_id')]['h1_home'];
-		if ($this->config->get('revtheme_geo_set')['status']) {
-			require_once(DIR_SYSTEM . 'library/revolution/SxGeo.php');
-			$SxGeo = new SxGeo();
-			if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-				$ip = $_SERVER['HTTP_CLIENT_IP'];
-			} else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-				$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-			} else {
-				$ip = $_SERVER['REMOTE_ADDR'];
-			}
-			$ip_city = $SxGeo->getCity($ip)['city']['name_ru'];
-			$ip_region = $SxGeo->getCityFull($ip)['region']['name_ru'];
-			$rev_geo_data = $this->config->get('revtheme_geo');
-			$data['rev_geos'] = array();
-			if (!empty($rev_geo_data)){
-				foreach ($rev_geo_data as $rev_geo) {
-					if ($ip_city == $rev_geo['city'] || $ip_region == $rev_geo['city']) {
-						$data['rev_geos'][] = array(
-							'code' => $rev_geo['code'],
-							'text' => $rev_geo['text'][$this->config->get('config_language_id')]
-						);
-					}
-				}
-			}
-			foreach ($data['rev_geos'] as $rev_geo) {
-				if (strpos($data['h1_home'], $rev_geo['code'])) {
-					$data['h1_home'] = html_entity_decode(str_replace($rev_geo['code'], $rev_geo['text'], $data['h1_home']), ENT_QUOTES, 'UTF-8');
-				}
-			}
-		}
 		$data['setting_home_slideshow'] = $this->config->get('revtheme_home_slideshow');
 		if ($this->config->get('revtheme_all_settings')['mobile_on']) {
 			$is_mobile = $data['is_mobile'] = $this->mobiledetect->isMobile();
