91f02b880c6eb9f042327e5389703dda2c149ac1
Nikita Markin <mr.nik2014@gmail.com>
2025-09-10 13:34:01 +0300

    REFACTOR: cache module_settings

--- system/library/trait/module_settings.php
+++ system/library/trait/module_settings.php
@@ -1,38 +1,56 @@
 <?php
+require_once(DIR_SYSTEM . 'library/trait/module_settings.php');
+
 trait TraitModuleSettings {
+  use CacheTrait;
     
-    // Сохранить настройки
-    protected function saveSettings($code, $settings) {
-        $settings_json = json_encode($settings);
-        
-        $this->db->query("INSERT INTO " . DB_PREFIX . "module_settings 
-                         SET `code` = '" . $this->db->escape($code) . "', 
-                             `setting` = '" . $this->db->escape($settings_json) . "'
-                         ON DUPLICATE KEY UPDATE 
-                             `setting` = '" . $this->db->escape($settings_json) . "'");
-    }
+  // Сохранить настройки
+  protected function saveSettings($code, $settings) {
+    $settings_json = json_encode($settings);
     
-    // Получить настройки по коду
-    protected function getSettings($code) {
-        $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "module_settings 
-                                  WHERE `code` = '" . $this->db->escape($code) . "'");
-        
-        if ($query->num_rows) {
-            return json_decode($query->row['setting'], true);
-        }
-        
-        return array();
-    }
+    $this->db->query("INSERT INTO " . DB_PREFIX . "module_settings 
+                      SET `code` = '" . $this->db->escape($code) . "', 
+                          `setting` = '" . $this->db->escape($settings_json) . "'
+                      ON DUPLICATE KEY UPDATE 
+                          `setting` = '" . $this->db->escape($settings_json) . "'");
+  }
     
-    protected function getSettingsByPrefix($prefix) {
-        $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "module_settings 
-                                  WHERE `code` LIKE '" . $this->db->escape($prefix) . "%'");
-        
-        $result = array();
-        foreach ($query->rows as $row) {
-            $result[$row['code']] = json_decode($row['setting'], true);
-        }
-        
-        return $result;
+  // Получить настройки по коду
+  protected function getSettings($code) {
+		$cache_key = 'module_settings.' . $code;
+		$cache = $this->getCache($cache_key);
+    if ($cache !== false) {
+      return $cache;
     }
+
+    $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "module_settings 
+                              WHERE `code` = '" . $this->db->escape($code) . "'");
+    
+    $result = array();
+    if ($query->num_rows) {
+      $result = json_decode($query->row['setting'], true);
+    }
+    $this->setCache($cache_key, $result);
+    
+    return $result;
+  }
+    
+  protected function getSettingsByPrefix($prefix) {
+		$cache_key = 'module_settings.by_prefix.' . $prefix;
+		$cache = $this->getCache($cache_key);
+    if ($cache !== false) {
+      return $cache;
+    }
+
+    $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "module_settings 
+                              WHERE `code` LIKE '" . $this->db->escape($prefix) . "%'");
+    
+    $result = array();
+    foreach ($query->rows as $row) {
+      $result[$row['code']] = json_decode($row['setting'], true);
+    }
+    $this->setCache($cache_key, $result);
+    
+    return $result;
+  }
 }
