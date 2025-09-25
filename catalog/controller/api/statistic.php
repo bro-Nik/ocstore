a708fc8511a82b4fc051da319a5ff92542073601
Nikita Markin <mr.nik2014@gmail.com>
2025-09-25 17:05:17 +0300

    FIX: statistic controller

--- catalog/controller/api/statistic.php
+++ catalog/controller/api/statistic.php
@@ -3,11 +3,11 @@ class ControllerApiStatistic extends Controller {
   public function pageViewCounter() {
     $this->load->model('catalog/statistic');
     
-    if (isset($this->request->get['type']) && isset($this->request->get['id'])) {
-      $type = $this->request->get['type'];
-      $id = (int)$this->request->get['id'];
+    if (isset($this->request->post['type']) && isset($this->request->post['id'])) {
+      $type = $this->request->post['type'];
+      $id = (int)$this->request->post['id'];
 
-      $this->model_catalog_counter->pageViewCounter($type, $id);
+      $this->model_catalog_statistic->pageViewCounter($type, $id);
     }
   }
 }
--- catalog/model/catalog/statistic.php
+++ catalog/model/catalog/statistic.php
@@ -4,17 +4,15 @@ class ModelCatalogStatistic extends Model {
     $valid_types = ['product', 'category', 'manufacturer', 'ocfilter_page'];
     
     if (!in_array($type, $valid_types) || $id <= 0) {
-      return 'Не прошло';
+      return;
     }
     
     $table = $type;
     $field = $type === 'ocfilter_page' ? 'page' : $type;
     $field = $field . '_id';
-    
+
     $this->db->query("UPDATE " . DB_PREFIX . $table . "
-                      SET viewed = viewed * 0.9 + 1 
+                      SET viewed = viewed + 1 
                     	WHERE " . $field . " = '" . (int)$id . "'");
-    
-    return 'Прошло';
   }
 }
